8be286852d6131212322ba08dfce64886a76f5e9
sungwon.cha <mygenie3@naver.com>
2018-07-21 01:37:33 +0900

    fix

--- stories/examples/index.js
+++ stories/examples/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs/react';
-import Calendar, { withRange } from '../../src2';
+import Calendar, { withRange } from '../../src';
 // import Wizard from '../../src/extensions/Wizard';
 import schema from '../assets/schema';
 import WithBootstrap from '../WithBootstrap';
